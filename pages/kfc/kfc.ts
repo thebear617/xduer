89d032a776812ad56931686d3ca35928b562947d
thebear617 <98879632+thebear617@users.noreply.github.com>
2022-04-23 13:52:44 +0800

    Add files via upload

---
+++ pages/kfc/kfc.ts
@@ -0,0 +1,84 @@
+// pages/kfc/kfc.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  dingxiang_1:function(){
+    wx.navigateTo({url:'/pages/dingxiang_1/dingxaing_1'})
+  },
+  dingxiang_2:function(){
+   wx.navigateTo({url:'/pages/dingxiang_2/dingxaing_2'})
+ },
+ haitang_1:function(){
+   wx.navigateTo({url:'/pages/haitang_1/haitang_1'})
+ },
+ haitang_2:function(){
+   wx.navigateTo({url:'/pages/haitang_2/haitang_2'})
+ },
+ zhuyuan_1:function(){
+   wx.navigateTo({url:'/pages/zhuyuan_1/zhuyuan_1'})
+ },
+ zhuyuan_2:function(){
+   wx.navigateTo({url:'/pages/zhuyuan_2/zhuyuan_2'})
+ },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
